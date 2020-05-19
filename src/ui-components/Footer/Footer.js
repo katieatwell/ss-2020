3b67ca9913b6690f19b2e85b0ca95b55faaba86c
Katie Atwell <katieatwell@outlook.com>
2020-05-19 11:53:18 -0400

    Update links

--- src/ui-components/Footer/Footer.js
+++ src/ui-components/Footer/Footer.js
@@ -31,14 +31,11 @@ function Footer({ className }) {
               style={{ marginBottom: "2rem" }}
             />
             <Box display="flex" justifyContent="space-between">
-              <a
-                href="https://www.facebook.com/SummitSeltzerCo"
-                target="_blank"
-              >
+              <a href="https://www.facebook.com/summitseltzery" target="_blank">
                 <Typography>facebook</Typography>
               </a>
               <a
-                href="https://www.instagram.com/summitseltzer/"
+                href="https://www.instagram.com/summitseltzery/"
                 target="_blank"
               >
                 <Typography>instagram</Typography>
